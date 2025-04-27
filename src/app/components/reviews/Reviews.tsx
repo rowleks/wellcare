ec683c2d5fed7bbe6f22f32792d34e659bad2ca4
rowleks <momohrowland@gmail.com>
2025-04-27 16:53:58 +0100

    reviews section done

--- src/app/components/reviews/Reviews.tsx
+++ src/app/components/reviews/Reviews.tsx
@@ -1,14 +1,40 @@
-import Link from "next/link";
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 import ArrowLeft from "../buttons/ArrowLeft";
 import ArrowRight from "../buttons/ArrowRight";
 import Heading from "../headings/Heading";
-import ReviewsCard from "./ReviewsCard";
+import ReviewsCard, { ReviewType } from "./ReviewsCard";
+
+const reviewsData: ReviewType[] = [
+  {
+    imageSrc: "/review-1.png",
+    message:
+      "I had an exceptional experience with WellCare. The expert doctors provided me with personalized care, and the friendly staff made me feel comfortable throughout. I'm truly grateful for their dedicated service!",
+    author: "Wade Warren",
+  },
+  {
+    imageSrc: "/review-2.png",
+    message:
+      "I'm truly grateful for the exceptional care I received at WellCare. The dedicated team went above and beyond to address my health concerns and guide me toward a better lifestyle.",
+    author: "Leslie Alexander",
+  },
+  {
+    imageSrc: "/review-3.png",
+    message:
+      "Choosing WellCare for my surgical procedure was the best decision I made. The level of professionalism and care I experienced from start to finish exceeded my expectations and coaches at WellCare are dedicated and knowledgeable.",
+    author: "Bessie Cooper",
+  },
+];
 
 export default function Reviews() {
   const headingContents = {
     title: "patient experience",
     subtitle: "reviews",
   };
+
   return (
     <section>
       <div className="wrapper">
@@ -18,16 +44,32 @@ export default function Reviews() {
               <Heading contents={headingContents} />
             </div>
             <div className="hidden md:flex items-center gap-3">
-              <Link href="#" className="group">
+              {/* Custom navigation buttons */}
+              <div className="group swiper-button-prv">
                 <ArrowLeft />
-              </Link>
-              <Link href="#" className="group">
+              </div>
+              <div className="group swiper-button-nxt">
                 <ArrowRight />
-              </Link>
+              </div>
             </div>
           </div>
           <div>
-            <ReviewsCard />
+            <Swiper
+              modules={[Navigation]}
+              navigation={{
+                prevEl: ".swiper-button-prv",
+                nextEl: ".swiper-button-nxt",
+              }}
+              spaceBetween={30}
+              slidesPerView={1}
+              loop={true}
+            >
+              {reviewsData.map((review, index) => (
+                <SwiperSlide key={index}>
+                  <ReviewsCard review={review} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
--- src/app/components/reviews/ReviewsCard.tsx
+++ src/app/components/reviews/ReviewsCard.tsx
@@ -1,28 +1,33 @@
 import Image from "next/image";
 
-export default function ReviewsCard() {
+export interface ReviewType {
+  imageSrc: string;
+  message: string;
+  author: string;
+}
+
+type ReviewProps = {
+  review: ReviewType;
+};
+
+export default function ReviewsCard({ review }: ReviewProps) {
   return (
-    <>
-      <div className="flex flex-col md:flex-row items-center gap-7 md:gap-12 lg:gap-18">
-        <div className="bg-secondary-100 md:max-w-2/5 lg:max-w-3/10 w-full h-auto flex-1">
-          <Image
-            src="/review-1.png"
-            alt="Wade Warren"
-            width={896}
-            height={920}
-            className="object-cover"
-          />
-        </div>
-        <div className="flex flex-col gap-7 lg:gap-12 flex-1">
-          <p className="text-dark-200 text-lg md:text-2xl lg:text-[1.625rem] font-light leading-relaxed">
-            &#34;I had an exceptional experience with WellCare. The expert
-            doctors provided me with personalized care, and the friendly staff
-            made me feel comfortable throughout. I&#39;m truly grateful for
-            their dedicated service!&#34;
-          </p>
-          <p className="text-lg font-urbanist font-medium">- Wade Warren</p>
-        </div>
+    <div className="flex flex-col md:flex-row items-center gap-7 md:gap-12 lg:gap-18 select-none">
+      <div className="bg-secondary-100 md:max-w-2/5 lg:max-w-3/10 w-full h-auto flex-1">
+        <Image
+          src={review.imageSrc}
+          alt={review.author}
+          width={896}
+          height={920}
+          className="object-cover"
+        />
       </div>
-    </>
+      <div className="flex flex-col gap-7 lg:gap-12 flex-1">
+        <p className="text-dark-200 text-lg md:text-2xl lg:text-[1.625rem] font-light leading-relaxed cursor-grabbing">
+          &#34;{review.message}&#34;
+        </p>
+        <p className="text-lg font-urbanist font-medium">- {review.author}</p>
+      </div>
+    </div>
   );
 }
